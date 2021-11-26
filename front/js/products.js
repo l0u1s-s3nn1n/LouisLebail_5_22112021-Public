91212bfd818e8cba9047fcdd6622d41ec39a9436
Louis <louislebail@protonmail.com>
2021-11-26 10:37:16 +0100

    JS ( index.js + product.js + products.js)

--- front/js/products.js
+++ front/js/products.js
@@ -1,10 +1,12 @@
+//fetch permettant de récupérer l'ensemble des données "produits" disponibles dans l'API, puis de les récupérer dans le fichier json.
+
 fetch('http://localhost:3000/api/products/')
     .then(response => {
         return response.json()
     })
     .then(data => {
         console.log(data);
-
+//Exploitation des données récupérer  dans l'API, envoyées dans le #items, puis dans leurs balises correspondantes.
         data.forEach(element => {
             document.querySelector('#items').innerHTML += '<a href="./product.html?id='+element._id+'">\n' +
                 '            <article>\n' +
